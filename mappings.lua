061ab8935750226ce5ec08b960fb13be98c1f658
Patricio Serrano <serranomorantepatricio@gmail.com>
2023-04-10 01:02:10 +0200

    Custom mappings for buffer navigation

--- mappings.lua
+++ mappings.lua
@@ -3,6 +3,10 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+
+local buffer = require "astronvim.utils.buffer"
+local astro_utils = require "astronvim.utils"
+
 return {
   -- first key is the mode
   n = {
@@ -11,7 +15,7 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) buffer.close(bufnr) end)
       end,
       desc = "Pick to close",
     },
@@ -20,6 +24,26 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    -- Disable default navigation bindings
+    ["[b"] = false,
+    ["b]"] = false,
+    -- Navigate through buffers
+    ["<S-l>"] = { function() buffer.nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next Buffer" },
+    ["<S-h>"] = { function() buffer.nav(-(vim.v.count > 0 and vim.v.count or 1)) end, desc = "Previous Buffer" },
+    -- Use telescope buffer browser
+    ["<Tab>"] = {
+      function()
+        if #vim.t.bufs > 1 then
+          require("telescope.builtin").buffers {
+            sort_mru = true,
+            ignore_current_buffer = true,
+          }
+        else
+          astro_utils.notify "No other buffers open"
+        end
+      end,
+      desc = "Switch Buffers",
+    },
   },
   t = {
     -- setting a mapping to false will disable it
