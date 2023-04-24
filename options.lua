77c14016fb8435487edd91f1bd129a7e824e7983
Patricio Serrano <serranomorantepatricio@gmail.com>
2023-04-24 14:26:31 +0200

    Disabled fish as default shell for vim

--- options.lua
+++ options.lua
@@ -6,7 +6,8 @@ return {
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = false, -- sets vim.opt.wrap
+    wrap = false, -- sets vim.opt.wrap,
+    shell = "/bin/sh", -- sets vim.opt.shell
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
